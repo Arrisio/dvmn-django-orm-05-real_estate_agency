fbf5e3cd0288ecc4bba4397de258e546fa8667a2
Dmitriy Suchkov <Account.log@yandex.ru>
2021-09-09 17:15:59 +0300

    feat: 12/21 add normalized phone field

--- property/models.py
+++ property/models.py
@@ -2,10 +2,15 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+from phonenumber_field.modelfields import PhoneNumberField
+
 
 class Flat(models.Model):
     owner = models.CharField("ФИО владельца", max_length=200)
     owners_phonenumber = models.CharField("Номер владельца", max_length=20)
+    owner_pure_phone = PhoneNumberField(
+        "Номер владельца (нормализованный)", blank=True, max_length=20
+    )
     created_at = models.DateTimeField(
         "Когда создано объявление", default=timezone.now, db_index=True
     )
@@ -46,7 +51,9 @@ class Flat(models.Model):
 
     new_building = models.NullBooleanField("Новостройка", default=None)
 
-    liked_by = models.ManyToManyField(User, related_name='liked_flats')
+    liked_by = models.ManyToManyField(
+        User, related_name="liked_flats", blank=True, verbose_name="Кто лайкнул"
+    )
 
     def __str__(self):
         return f"{self.town}, {self.address} ({self.price}р.)"
