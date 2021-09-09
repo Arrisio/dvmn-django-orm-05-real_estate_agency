5d2170e1df04e8a3f106c9546dd05a7e75db7585
Dmitriy Suchkov <Account.log@yandex.ru>
2021-09-09 16:21:47 +0300

    feat: 10/21 add complain

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Flat
+from .models import Flat, Complaint
 
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
@@ -10,4 +10,7 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ['new_building']
     list_filter = ['new_building']
 
-# admin.site.register(Flat)
+@admin.register(Complaint)
+class ComplaintAdmin(admin.ModelAdmin):
+    raw_id_fields =['complainant', 'flat']
+    list_display = ['complainant', 'flat', 'text']
