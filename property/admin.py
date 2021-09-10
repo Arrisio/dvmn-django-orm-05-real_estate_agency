cf7d0139ebb9ed345d3d6395a7a899a692ad1b89
Dmitriy Suchkov <Account.log@yandex.ru>
2021-09-10 17:12:20 +0300

    feat: register OwnerAdmin

--- property/admin.py
+++ property/admin.py
@@ -1,18 +1,31 @@
 from django.contrib import admin
 
-from .models import Flat, Complaint
+from .models import Flat, Complaint, Owner
+
 
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ['address', 'town', 'town_district']
+    search_fields = ["address", "town", "town_district"]
     readonly_fields = ["created_at"]
-    list_display = ['address','price','construction_year','town','new_building']
-    list_editable = ['new_building']
-    list_filter = ['new_building']
-    raw_id_fields = ['liked_by']
+    list_display = [
+        "address",
+        "price",
+        "construction_year",
+        "town",
+        "new_building",
+    ]
+    list_editable = ["new_building"]
+    list_filter = ["new_building"]
+    raw_id_fields = ["liked_by"]
+
 
 @admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
-    raw_id_fields =['complainant', 'flat']
-    list_display = ['complainant', 'flat', 'text']
+    raw_id_fields = ["complainant", "flat"]
+    list_display = ["complainant", "flat", "text"]
 
+
+@admin.register(Owner)
+class OwnerAdmin(admin.ModelAdmin):
+    raw_id_fields = ["flat"]
+    list_display = ["name"]
